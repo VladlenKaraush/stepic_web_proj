49aa4827581cc8556dab38d3b6d68b8cf0edd0d2
vladlen <vladlenkaraush@mail.ru>
2016-11-10 20:03:33 +0300

    First commition for the second stepic task

---
+++ hello.py
@@ -0,0 +1,12 @@
+from cgi import parse_qs
+
+def application(environ, start_response):
+
+	query = parse_qs(environ['QUERY_STRING'], keep_blank_values=True)
+	status = '200 OK'
+	headers = [
+	('Content-type', 'text-plain')
+	]
+	
+	start_response(status,headers)
+	return '\n'.join(query.split('&'))
