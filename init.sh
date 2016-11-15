0f2725507e8c3482b435bb18c5af2e9eba2c6dcc
vladlen <vladlenkaraush@mail.ru>
2016-11-15 20:53:29 +0300

    init.sh changed, now it also runs django project on 8000 port

--- init.sh
+++ init.sh
@@ -1,4 +1,6 @@
 
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-gunicorn -b 0.0.0.0:8080 hello:application
+gunicorn -b 0.0.0.0:8080 hello:application &
+cd ask
+gunicorn -b 0.0.0.0:8000 ask.wsgi
