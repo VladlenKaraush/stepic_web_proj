d2c946eb049035aee6dace3dc8e82a77e0b6b0a8
vladlen <vladlenkaraush@mail.ru>
2016-11-15 21:32:47 +0300

    Commented several middleware classes in settings.py

--- hello.py
+++ hello.py
@@ -2,11 +2,9 @@ from cgi import parse_qs
 
 def application(environ, start_response):
 
-	query = parse_qs(environ['QUERY_STRING'], keep_blank_values=True)
 	status = '200 OK'
 	headers = [
 	('Content-type', 'text-plain')
 	]
-	
 	start_response(status,headers)
-	return '\n'.join(query.split('&'))
+	return ["\n".join(environ["QUERY_STRING"].split("&"))]
