3a9319ba981b81dbbcb7e99a4856924b0f65a1ed
Abra <a@abra.me>
2016-12-22 23:59:49 +0300

    sd

--- hello.py
+++ hello.py
@@ -1,8 +1,9 @@
 from cgi import parse_qs
 
 def application(environ, start_response):
-
+        #just comment for commiting
 	status = '200 OK'
+        #here i declare headers of returning http request
 	headers = [
 	('Content-type', 'text-plain')
 	]
