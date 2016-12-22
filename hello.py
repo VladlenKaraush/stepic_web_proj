7149756ec8494b350e9ea00b2f42046efba56ede
Abra <a@abra.me>
2016-12-23 00:04:36 +0300

    Finally commit from vim via Plugin 'fugitive'

--- hello.py
+++ hello.py
@@ -1,9 +1,7 @@
 from cgi import parse_qs
-
+#comments deleted
 def application(environ, start_response):
-        #just comment for commiting
 	status = '200 OK'
-        #here i declare headers of returning http request
 	headers = [
 	('Content-type', 'text-plain')
 	]
